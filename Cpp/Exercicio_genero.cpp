78134f67c7ff897ad0b096cc813fd2222a263f65
Kemily Teixeira <101020416+kmlyteixeira@users.noreply.github.com>
2022-03-06 21:36:31 -0300

    Add files via upload

---
+++ Cpp/Exercicio_genero.cpp
@@ -0,0 +1,40 @@
+/*FUP que solicite uma letra informando o SEXO e imprima as mensagens correspondentes
+EXERCICIO AULA 02/03/2022
+ALUNA: Kemily Teixeira
+*/
+
+#include <iostream> 
+
+using namespace std;
+main()
+{
+    char sexo;
+
+    setlocale(LC_ALL, "Portuguese");
+    cout << "\n\t--------PESQUISA DE GENERO--------";
+    cout << "\n\t-----------[F]FEMININO------------";
+    cout << "\n\t-----------[M]MASCULINO-----------";
+    cout << "\n\t-----------[N]NAO INFORMAR--------";
+
+    cout << "\n\n\n\t Informe seu SEXO: \t";
+    cin >> sexo;
+
+    if ((sexo == 'F') || (sexo == 'f'))
+    {
+        cout << "\n\n\t SEXO [FEMININO] SELECIONADO";
+    }
+    else if ((sexo == 'M') || (sexo == 'm'))
+    {
+        cout << "\n\n\t SEXO [MASCULINO] SELECIONADO";
+    }
+    else if ((sexo == 'N') || (sexo == 'n'))
+    {
+        cout << "\n\n\t SEXO NAO INFORMADO";
+    }
+    else
+    {
+        cout << "\n\n\t FAVOR INFORMAR UMAS DAS OPCOES \
+               \n\n\t [F] ou [M] ou [N]";
+    }
+    cout << "\n\n\n\n\n\n";
+}
