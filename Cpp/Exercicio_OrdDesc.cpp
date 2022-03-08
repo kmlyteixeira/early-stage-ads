4f3df3b703fcbcdae508cb62f4f0ffb8da353165
Kemily Teixeira <101020416+kmlyteixeira@users.noreply.github.com>
2022-03-08 13:15:57 -0300

    Update Exercicio_OrdDesc.cpp

--- Cpp/Exercicio_OrdDesc.cpp
+++ Cpp/Exercicio_OrdDesc.cpp
@@ -1,49 +1,44 @@
-/*FUP   que   leia   três   números   e   mostre-os   em   ordem decrescente. 
+/*FUP   que   leia   trÃªs   nÃºmeros   e   mostre-os   em   ordem decrescente. 
 Aula: 07/03/2022
-Aluna: Kemily
-
-
+Aluna: Kemily */
 #include <iostream> 
 #include <iomanip> 
 using namespace std; 
-
 main()
 {	
 	setlocale(LC_ALL, "Portuguese");
 	system ("color B");	
 		
-	int number1, number2, number3;
-	char primeiro, segundo, terceiro;
+	int ordenador, number1, number2, number3;
 	
 	cout<< "\n\t ==========================================";	
 	cout<< "\n\t =======NUMEROS EM ORDEM DECRESCENTE=======";
 	cout<< "\n\t =========================================="; 
 	
-	cout<< "\n\n\n\t Insira o 1ª número:";
+	cout<< "\n\n\n\t Insira o 1Âª nÃºmero:";
 	cin>> number1; 							
-	cout<< "\n\n\n\t Insira o 2ª número:";
+	cout<< "\n\n\n\t Insira o 2Âª nÃºmero:";
 	cin>> number2;
-	cout<< "\n\n\n\t Insira o 3ª número:";
+	cout<< "\n\n\n\t Insira o 3Âª nÃºmero:";
 	cin>> number3; 							
 	 							
-	if(number1>number3)&&(number1<number2){
-		number1 = primeiro;	
-		number2 = segundo;
-		number3 = terceiro;
-	} else if (number1>number2) {
-		number1 = terceiro;
-		
-	} else {
-		number1 = segundo;
-	}
+    if(number2 < number1){
+        ordenador = number1;
+        number1 = number2;
+        number2 = ordenador;
+    }
+
+    if(number3 < number1){
+        ordenador = number1;
+        number1 = number3;
+        number3 = ordenador;
+    }
+
+    if(number3 < number2){
+        ordenador = number2;
+        number2 = number3;
+        number3 = ordenador;
+    }
 	
-	if(number1>number3)&&(number1<number2){
-		number1 = primeiro;	
-	} else if (number1>number2) {
-		number1 = terceiro;
-	} else {
-		number1 = segundo;
-	}
-	
-	
-}*/
+	cout <<"\n\n\tOrdenando seus nÃºmeros em Ordem Decrescente: \n\t "<<number3<<", "<<number2<<", "<<number1;
+}
