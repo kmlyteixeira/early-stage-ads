0fc56da84e61f4ab1e6d4c66c7461ce64ea8c47f
Kemily Teixeira <101020416+kmlyteixeira@users.noreply.github.com>
2022-03-14 20:53:16 -0300

    Add files via upload

---
+++ Cpp/ExercNegParMult.cpp
@@ -0,0 +1,41 @@
+/*FUP altere o programa anterior para imprimir:
+•A quantidade de números negativos
+•A quantidade de pares
+•A quantidade de números múltiplos de 7
+Aluna: Kemily Teixeira
+Data: 14/03/2022
+*/
+
+#include <iostream> 
+#include <iomanip> 
+using namespace std; 
+
+main()
+{	
+	int numero,negativos,pares,multiplosSete;
+	
+	setlocale(LC_ALL, "Portuguese"); 	
+	system ("color B");						
+	
+	cout<< "\n\t--------------------------------------------------------";
+	cout<< "\n\t------------VERIFICANDO NUMEROS NEGATIVOS---------------";
+	cout<< "\n\t--------------------------------------------------------";
+	
+	cout<< "\n\n\t Insira [10] números: \n";
+	for (int i=0;i<10;i++){
+		cin>> numero;
+		if(numero<0){
+			negativos++;
+			}
+		if(numero%2==0){
+			pares++;
+			}
+		if(numero/7==7){
+			multiplosSete++;
+			}
+		}
+			
+	cout<< "\t"<< negativos<< " são negativos!";
+	cout<< "\t"<< pares << " são pares!";
+	cout<< "\t"<< multiplosSete<< " são multiplos de SETE!";
+}
