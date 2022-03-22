3392a2841a41e22a94d8df06277baa6d19ae47ec
Kemily Teixeira <101020416+kmlyteixeira@users.noreply.github.com>
2022-03-22 13:32:01 -0300

    Update ExercMediasFOR.cpp

--- Cpp/ExercMediasFOR.cpp
+++ Cpp/ExercMediasFOR.cpp
@@ -1,9 +1,9 @@
 /*FUP que leia a idade, o sexo e a altura de N pessoas, onde N pessoas
-dever· ser o limite da repetiÁ„o do FOR e dever· ser solicitado ao usu·rio. 
-ApÛs a entrada dos valores o prgrama dever· informar:
+dever√° ser o limite da repeti√ß√£o do FOR e dever√° ser solicitado ao usu√°rio. 
+Ap√≥s a entrada dos valores o prgrama dever√° informar:
 
-A mÈdia de todas as idades e alturas.
-A mÈdia das alturase das idades das mulheres e dos homens.
+A m√©dia de todas as idades e alturas.
+A m√©dia das alturase das idades das mulheres e dos homens.
 A quantidade demulheres acima de 1,70 m.
 A quantidade de homens acima de 1,90 m.
 A quantidade de homens abaixo de 1,70 m.
@@ -21,76 +21,108 @@ using namespace std;
 
 main()
 {		
-	int n, idadeFem, idadeMasc, acumIdadeFem, acumIdadeMasc,contIdadeFem,
-		contIdadeMasc,contHFem,contHMasc,contFem170,contMasc170,contMasc190,maiorIdadeFem,menorIdadeFem,maiorIdadeMasc,menorIdadeMasc;
+	int n = 0, 
+	idadeFem = 0, 
+	idadeMasc = 0, 
+	acumIdadeFem = 0, 
+	acumIdadeMasc = 0,
+	contIdadeFem = 0,
+	contIdadeMasc = 0,
+	contHFem = 0,
+	contHMasc = 0,
+	contFem170 = 0,
+	contMasc170 = 0,
+	contMasc190 = 0,
+	maiorIdadeFem = 0,
+	menorIdadeFem = 0,
+	maiorIdadeMasc = 0,
+	menorIdadeMasc = 0;
+	
 	char sexo;
-	float alturaFem, alturaMasc, acumHFem, acumHMasc, mediaIdades, 
-		  mediaH, mediaHMasc, mediaIdadeMasc,mediaHFem,mediaIdadeFem;
+	
+	float 
+	alturaFem = 0, 
+	alturaMasc = 0, 
+	acumHFem = 0, 
+	acumHMasc = 0, 
+	mediaIdades = 0, 
+	mediaH = 0, 
+	mediaHMasc = 0, 
+	mediaIdadeMasc = 0,
+	mediaHFem = 0,
+	mediaIdadeFem = 0;
 	
 	setlocale(LC_ALL, "Portuguese"); 	
 	system ("color B");						
 	
 	cout<< "\n\t-------------------------------------------";
-	cout<< "\n\t--------------PROGRAMA M…DIAS--------------";
+	cout<< "\n\t--------------PROGRAMA M√âDIAS--------------";
 	cout<< "\n\t-------------------------------------------\n\n";
 	
 	cout<< "\n\tQual a quantidade de pessoas que deseja informar? \t";
 	cin>> n;
 	
 	for (int i=1;i<=n;i++){
-		cout<< "\n\t Comece informando o sexo [F]Fem [M]Masc: \t";
+		cout<< "\n\t Comece informando o sexo  da "<<i<<" pessoa [F]Fem [M]Masc: \t";
 		cin>> sexo;
 		sexo = toupper(sexo);
 		
 			if (sexo=='F'){
 				cout<< "\n\t Agora insira a idade desta mulher: \t";
 				cin>> idadeFem;
+				cout<< "\n\t E por √∫ltimo... insira a altura: \t";
+				cin>> alturaFem;
+			}
+					
+			if(sexo=='M'){
+				cout<< "\n\t Agora insira a idade deste homem: \t";
+				cin>> idadeMasc;
+				cout<< "\n\t E por √∫ltimo... insira a altura: \t";
+				cin>> alturaMasc;
+			}
+					
 				acumIdadeFem = acumIdadeFem + idadeFem;
 				contIdadeFem++;
-				
-				cout<< "\n\t E por ˙ltimo... insira a altura: \t";
-				cin>> alturaFem;
 				acumHFem = acumHFem + alturaFem;
 				contHFem++;
 					if(alturaFem> 1.70){
 						contFem170++;
 					}
-				
-			}
-					if(sexo=='M') {
-						cout<< "\n\t Agora insira a idade deste homem: \t";
-						cin>> idadeMasc;
-						acumIdadeMasc = acumIdadeMasc + idadeMasc;
-						contIdadeMasc++;
 					
-					cout<< "\n\t E por ˙ltimo... insira a altura: \t";
-					cin>> alturaMasc;
-					acumHMasc = acumHMasc + alturaMasc;
-					contHMasc++;
-						if(alturaMasc<1.70){
-							contMasc170++;
-							} if(alturaMasc>1.90){
-								contMasc190++;
-								}			 
-					}
+		acumIdadeMasc = acumIdadeMasc + idadeMasc;
+		contIdadeMasc++;
+				
+		acumHMasc = acumHMasc + alturaMasc;
+		contHMasc++;
+		
+		if(alturaMasc<1.70){
+			contMasc170++;
+		} 
+		if(alturaMasc>1.90){
+			contMasc190++;
+		}	
 					
 		if((i==1)&&(sexo=='F')){
-				maiorIdadeFem = idadeFem;
-				menorIdadeFem = idadeFem;
-					} if (maiorIdadeFem<idadeFem){
-						maiorIdadeFem=idadeFem;
-						} if (menorIdadeFem>idadeFem){
-							menorIdadeFem=idadeFem;
-							}
+			maiorIdadeFem = idadeFem;
+			menorIdadeFem = idadeFem;
+				} 
+				if (maiorIdadeFem<idadeFem){
+					maiorIdadeFem=idadeFem;
+				}
+					if (menorIdadeFem>idadeFem){
+					    menorIdadeFem=idadeFem;
+					}
 							
-			if((i==1)&&(sexo=='M')){
-				maiorIdadeMasc = idadeMasc;
-				menorIdadeMasc = idadeMasc;
-					} if (maiorIdadeMasc<idadeMasc){
-						maiorIdadeMasc=idadeMasc;
-						} if (menorIdadeMasc>idadeMasc){
-							menorIdadeMasc=idadeMasc;
-							}
+		if((i==1)&&(sexo=='M')){
+			maiorIdadeMasc = idadeMasc;
+			menorIdadeMasc = idadeMasc;
+				} 
+				if (maiorIdadeMasc<idadeMasc){
+					maiorIdadeMasc=idadeMasc;
+				}
+					if (menorIdadeMasc>idadeMasc){
+						menorIdadeMasc=idadeMasc;
+					}
 	}
 	
 	mediaIdades = (acumIdadeMasc + acumIdadeFem)/n;
@@ -102,24 +134,24 @@ main()
 	mediaHFem = acumHFem/contHFem;
 	mediaIdadeFem = acumIdadeFem/contIdadeFem;
 	
-	cout<< "\n\t A mÈdia de todas as idades È " << mediaIdades;
-	cout<< "\n\t E a mÈdia de todas as alturas È " << mediaH ;
+	cout<< "\n\t A m√©dia de todas as idades √© " << mediaIdades;
+	cout<< "\n\t E a m√©dia de todas as alturas √© " << mediaH ;
 	
-	cout<< "\n\n\t A mÈdia da idade dos homens È " << mediaIdadeMasc;
-	cout<< "\n\t e a mÈdia de suas alturas È " << mediaHMasc;
+	cout<< "\n\n\t A m√©dia da idade dos homens √© " << mediaIdadeMasc;
+	cout<< "\n\t e a m√©dia de suas alturas √© " << mediaHMasc;
 	
-	cout<< "\n\n\t A mÈdia da idade das mulheres È " << mediaIdadeFem;
-	cout<< "\n\t e a mÈdia de suas alturas È " << mediaHFem;
+	cout<< "\n\n\t A m√©dia da idade das mulheres √© " << mediaIdadeFem;
+	cout<< "\n\t e a m√©dia de suas alturas √© " << mediaHFem;
 	
-	cout<< "\n\n\t A quantidade de mulheres com altura acima de 1.70 È: " << contFem170;
-	cout<< "\n\t A quantidade de homens com altura abaixo de 1.70 È: " << contMasc170;
-	cout<< "\n\t A quantidade de homens com altura acima de 1.90 È: " << contMasc190;
+	cout<< "\n\n\t A quantidade de mulheres com altura acima de 1.70 √©: " << contFem170;
+	cout<< "\n\t A quantidade de homens com altura abaixo de 1.70 √©: " << contMasc170;
+	cout<< "\n\t A quantidade de homens com altura acima de 1.90 √©: " << contMasc190;
 	
-	cout<< "\n\n\t A maior idade entre os homens È: " << maiorIdadeMasc;
-	cout<< "\n\t A menor idade entre os homens È: " << menorIdadeMasc;
+	cout<< "\n\n\t A maior idade entre os homens √©: " << maiorIdadeMasc;
+	cout<< "\n\t A menor idade entre os homens √©: " << menorIdadeMasc;
 	
-	cout<< "\n\n\t A maior idade entre as mulheres È: " << maiorIdadeFem;
-	cout<< "\n\t A menor idade entre as mulheres È: " << menorIdadeFem;
+	cout<< "\n\n\t A maior idade entre as mulheres √©: " << maiorIdadeFem;
+	cout<< "\n\t A menor idade entre as mulheres √©: " << menorIdadeFem;
 		
 	
 }
