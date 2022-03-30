d7409c1c058c13ede11a81efe4f628b9c7c2fb95
Kemily Teixeira <101020416+kmlyteixeira@users.noreply.github.com>
2022-03-30 13:36:31 -0300

    Update ExercSwitchMath.cpp

--- Cpp/ExercSwitchMath.cpp
+++ Cpp/ExercSwitchMath.cpp
@@ -1,19 +1,19 @@
-/*FUP que tenha a seguinte lista de opções:
-[1] - Calcule um fatorial de um número. Ex.: 5!= 5*4*3*2*1
-[2] - Imprima os 10 primeiros termos da Série de Fibonacci. 
+/*FUP que tenha a seguinte lista de opÃ§Ãµes:
+[1] - Calcule um fatorial de um nÃºmero. Ex.: 5!= 5*4*3*2*1
+[2] - Imprima os 10 primeiros termos da SÃ©rie de Fibonacci. 
 Ex.: 
     1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...  
-[3] - Leia um número equivalente ao mês e imprima a quantidade de dias deste mês.
+[3] - Leia um nÃºmero equivalente ao mÃªs e imprima a quantidade de dias deste mÃªs.
 [4] - Leia 3 valores e imprima a soma dos 2 maiores.
-[5] - Calcule e imprima o MMC (Minímo Múltiplo Comum) de um número.
-[6] - Verifique se a data informada é válida. Leia a data no formato:
-      dd (dia) - mm (mês) - aaaa (ano)
+[5] - Calcule e imprima o MMC (MinÃ­mo MÃºltiplo Comum) de um nÃºmero.
+[6] - Verifique se a data informada Ã© vÃ¡lida. Leia a data no formato:
+      dd (dia) - mm (mÃªs) - aaaa (ano)
 [7] - FIM*/
 
 #include <iostream> 
 #include <iomanip> 
 using namespace std; 
-int i, opcao, numero, fat = 1, ultimo, penultimo, prox; 
+int i, opcao, numero, fat = 1, ultimo, penultimo, prox, mes, qntDias, numeroMaior; 
 
 main()
 {
@@ -22,80 +22,107 @@ main()
  do{
  	 system ("cls");
 	 cout<<" \n\t===========================================  "; 
-	 cout<<" \n\t\a  OPERAÇÕES MATEMÁTICAS - MENU SWITCH ";
+	 cout<<" \n\t\a  OPERAÃ‡Ã•ES MATEMÃTICAS - MENU SWITCH ";
 	 cout<<" \n\t===========================================  "; 
-	 cout<<"\n\n WELCOME :) >> Faça a sua escolha << ";
-	 cout<<"\n\n [1] - Calcular fatorial de um número.";
-	 cout<<"\n [2] - Imprimir os 10 primeiros termos da Série de Fibonacci";
-	 cout<<"\n [3] - Leia um número equivalente ao mês e imprima a quantidade de dias deste mês";
+	 cout<<"\n\n WELCOME :) >> FaÃ§a a sua escolha << ";
+	 cout<<"\n\n [1] - Calcular fatorial de um nÃºmero.";
+	 cout<<"\n [2] - Imprimir os 10 primeiros termos da SÃ©rie de Fibonacci";
+	 cout<<"\n [3] - Leia um nÃºmero equivalente ao mÃªs e imprima a quantidade de dias deste mÃªs";
 	 cout<<"\n [4] - Leia 3 valores e imprima a soma dos 2 maiores.";
-	 cout<<"\n [5] - Calcule e imprima o MMC (Minímo Múltiplo Comum) de um número.";
-	 cout<<"\n [6] - Verifique se a data informada é válida. Formato: DD/MM/YYYY";
+	 cout<<"\n [5] - Calcule e imprima o MMC (MinÃ­mo MÃºltiplo Comum) de um nÃºmero.";
+	 cout<<"\n [6] - Verifique se a data informada Ã© vÃ¡lida. Formato: DD/MM/YYYY";
 	 cout<<"\n [7] - Fim";
-	 cout<<"\n\n Insira uma das opções >>> \t ";
+	 cout<<"\n\n Insira uma das opÃ§Ãµes >>> \t ";
 	 cin>>opcao;
 	 switch (opcao)
 	 {
 	  	case 1:{
-	            cout<<"\n\n ------FATORIAL de um NÚMERO------";
+	            cout<<"\n\n ------FATORIAL de um NÃšMERO------";
 	            do{
-	            	cout<<"\n\n Escolha o número para calcular: ";
+	            	cout<<"\n\n Escolha o nÃºmero para calcular: ";
 	           		cin>>numero;
 		            	for (i=1;i<=numero;i++){
 		            		fat *= i; /*armazena o produto*/
 						}
-					cout<< "\n O Fatorial de " << numero << " é: " <<fat;
+					cout<< "\n O Fatorial de " << numero << " Ã©: " <<fat;
 				} while (numero);
 	            cout<<"\n\n";
 				system("pause");
 				break;
 	            }
-/*		            --- FINALIZAR A PARTIR DAQUI -------
+
 		case 2:{
-	            cout<<"\n\n Consultando os 10 primeiros termos da Série de Fibonacci";
+	            cout<<"\n\n Imprimindo os 10 primeiros termos da SÃ©rie de Fibonacci \n\t";
 	            ultimo = 1;
 	            penultimo = 1;
-		            for (i=0;i<=10;i++){
+		            for (i=0;i<8;i++){
 		            	if (i==0){
 		            		cout<<" "<< ultimo;
 		            		cout<<" "<< penultimo;	
 						}
-						
 						prox = ultimo+penultimo;
-						
-						cout<<" "<< prox;
-						penultimo = ultimo;
-						ultimo = prox;
+        				cout<<" "<< prox;
+        				penultimo = ultimo;
+        				ultimo = prox;
 					}
 	                
 	            cout<<"\n\n";
 				system("pause");
 	            break; 
 	            }
-            
+	            
 	    case 3 :{
-	            cout<<"\n\n Números ímpares de 0 a 200, utilizando comando DO_WHILE";
-	            cout<<"\n\n";
-	            i=0;
-	                do {
-	                    if (i%2!=0)
-	                    cout<<" | "<<i;
-	                    i++;
-	                } while (i<=200);
-	            cout<<"\n\n";
-				system("pause");
-	            break;
+	            cout<<"\n\n Leia um nÃºmero equivalente ao mÃªs e imprima a quantidade de dias deste mÃªs ";
+	            cout<<"\n\n Insira o numero que corresponte ao mÃªs desejado >>> \t";
+	            cout<<"\n [1] JANEIRO ";
+	            cout<<"\n [2] FEVEREIRO ";
+	            cout<<"\n [3] MARÃ‡O ";
+	            cout<<"\n [4] ABRIL ";
+	            cout<<"\n [5] MAIO ";
+	            cout<<"\n [6] JUNHO ";
+	            cout<<"\n [7] JULHO ";
+	            cout<<"\n [8] AGOSTO ";
+	            cout<<"\n [9] SETEMBRO ";
+	            cout<<"\n [10] OUTUBRO ";
+	            cout<<"\n [11] NOVEMBRO ";
+	            cout<<"\n [12] DEZEMBRO \n\t";
+	            cin>> mes;
+	            qntDias = 31;
+    	            switch(mes)
+    	            {
+    	                case 4: case 6: case 9: case 11:{
+    	                    qntDias= qntDias-1;
+    	                    break;
+    	                }
+    	                case 2: {
+    	                    qntDias=qntDias-2;
+    	                    break;
+    	                }
+    	                default : {
+    	                    qntDias = qntDias;
+    	                    break;
+    	                }
+    	            }
+    	            
+    	        cout<<"\n\n O mÃªs selecionado acima possui "<<qntDias<< " dias.";     
+    	        cout<<"\n\n";
+        		system("pause");
+        	    break;  
 	            }  
-				  
+ /*		            --- FINALIZAR A PARTIR DAQUI -------           				  
 	    case 4 :{
-	            cout<<"\n Sequência de números da sua escolha, utilizando o FOR";
-	            cout<<"\n Escolha o número de início: ";
-	            cin>>inicio;
-	            cout<<"\n Escolha o número final: ";
-	            cin>>fim;
-	            cout<<"\n\n";
-	                for (i=inicio;i<=fim;i++){
-	                   cout<<" | " << i;
+	            cout<<"\n Leia 3 valores e imprima a soma dos 2 maiores.";
+	                for (i=1;i<=3;i++){
+	                   cout<<"Insira o " <<i<< "Âº nÃºmero";
+	                   cin>> numero;
+	                        if(i==1){
+	                            numeroMaior=numero;
+	                        } if (numeroMaior>numero){
+	                            numeroMenor=numero;
+	                            numeroMaior=aux;
+	                        } if (numeroMaior<numero){
+	                            
+	                        }
 	                }
 	            cout<<"\n\n";
 				system("pause");
@@ -103,10 +130,10 @@ main()
 	            }
 	            
 	    case 5:{
-	            cout<<"\n Sequência de números da sua escolha, utilizando o DO_WHILE";
-	            cout<<"\n Escolha o número de início: ";
+	            cout<<"\n SequÃªncia de nÃºmeros da sua escolha, utilizando o DO_WHILE";
+	            cout<<"\n Escolha o nÃºmero de inÃ­cio: ";
 	            cin>>inicio;
-	            cout<<"\n Escolha o número final: ";
+	            cout<<"\n Escolha o nÃºmero final: ";
 	            cin>>fim;
 	            cout<<"\n\n";
 	            i=inicio;
@@ -120,10 +147,10 @@ main()
 	            }
 	
 	    case 6:{
-		        cout<<"\n Sequência de números da sua escolha, utilizando comando WHILE";
-		        cout<<"\n\n Escolha o número de início: ";
+		        cout<<"\n SequÃªncia de nÃºmeros da sua escolha, utilizando comando WHILE";
+		        cout<<"\n\n Escolha o nÃºmero de inÃ­cio: ";
 		        cin>>inicio;
-		        cout<<"\n Escolha o número final: ";
+		        cout<<"\n Escolha o nÃºmero final: ";
 		        cin>>fim;
 		        cout<<"\n\n";
 		        i=inicio;
@@ -138,7 +165,7 @@ main()
 	            
 	    case 7:{
 	            cout<<"\n\n Tabuada de um numero usando o WHILE";
-	            cout<<"\n\n Escolha o número para calcular: ";
+	            cout<<"\n\n Escolha o nÃºmero para calcular: ";
 	            cin>>tabuada;
 	                i=0;
 					while (i<=10)
@@ -152,7 +179,7 @@ main()
 	            }
 	
 	    default : {
-	            cout<<"\n  Escolha Inválida! Tente novamente >>> \n\n";
+	            cout<<"\n  Escolha InvÃ¡lida! Tente novamente >>> \n\n";
 	            cout<<"\n\n";
 	            system("pause");
 	            break;
