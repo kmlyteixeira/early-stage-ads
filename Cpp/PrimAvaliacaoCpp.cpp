b6314cd9d7e19ba68acc04197d6fa696546be615
Kemily Teixeira <101020416+kmlyteixeira@users.noreply.github.com>
2022-04-05 13:30:56 -0300

    Update PrimAvaliacaoCpp.cpp

--- Cpp/PrimAvaliacaoCpp.cpp
+++ Cpp/PrimAvaliacaoCpp.cpp
@@ -1,13 +1,10 @@
 /* 
 Faculdade SENAC - Joinville - SC
-Profª: Cláudia Werlich
-Atividade Avaliativa de Introdução a Programação de Computadores
-
+ProfÂª: ClÃ¡udia Werlich
+Atividade Avaliativa de IntroduÃ§Ã£o a ProgramaÃ§Ã£o de Computadores
 ALUNA: Kemily Teixeira da Rosa
-
 Data: 04/04/2022 
 (Altere a data conforme a entrega)
-
 */
 
 #include <iostream>
@@ -15,8 +12,8 @@ Data: 04/04/2022
 #include <iomanip>
 #include <conio.h> // Para usar o getch();
 
-int x,op, numero=0, inicio, fim;
-char escolha;
+int x,op, numero=0, inicio, fim, contador, aux;
+char escolha, repetir;
 
 using namespace std;
 main ()
@@ -26,41 +23,41 @@ system ("color B");
 system ("cls"); 
 cout << fixed << setprecision(2);
 
-   cout<<"ATIVIDADE AVALIATIVA DE INTRODUÇÃO A PROGRAMAÇÃO \n";
+   cout<<"ATIVIDADE AVALIATIVA DE INTRODUÃ‡ÃƒO A PROGRAMAÃ‡ÃƒO \n";
    cout<<"INDICADORES: \n";
-   cout<<"1. Realiza operações matemáticas de acordo com os fundamentos e o contexto apresentado.\n";
-   cout<<"2. Utiliza adequadamente os tipos de variáveis em algoritmos de acordo com o contexto.\n";
-   cout<<"3. Utiliza operações aritméticas em programas.\n";
+   cout<<"1. Realiza operaÃ§Ãµes matemÃ¡ticas de acordo com os fundamentos e o contexto apresentado.\n";
+   cout<<"2. Utiliza adequadamente os tipos de variÃ¡veis em algoritmos de acordo com o contexto.\n";
+   cout<<"3. Utiliza operaÃ§Ãµes aritmÃ©ticas em programas.\n";
    cout<<"4. Utiliza os comandos de leitura e escrita em algoritmos de acordo com o contexto.\n";
    cout<<"5. Utiliza as estruturas condicionais em algoritmos de acordo com o contexto.\n";
-   cout<<"6. Utiliza as estruturas de repetição em algoritmos de acordo com o contexto. \n";
-   cout<<"7. Utiliza vetores para solução de problemas computacionais em determinado contexto. \n";
+   cout<<"6. Utiliza as estruturas de repetiÃ§Ã£o em algoritmos de acordo com o contexto. \n";
+   cout<<"7. Utiliza vetores para soluÃ§Ã£o de problemas computacionais em determinado contexto. \n";
    
    do{
 	   system ("cls");
 	   cout<<"\n ====================================================================================";
-	   cout<<"\n [1] Questão 01 - Será liberada no dia 06/04/2022";
-	   cout<<"\n [2] Questão 02 - Será liberada no dia 06/04/2022";
-	   cout<<"\n [3] Questão 03 - Será liberada no dia 11/04/2022";
-	   cout<<"\n [4] Questão 04 - Será liberada no dia 11/04/2022";
-	   cout<<"\n [5] Questão 05 - Será liberada no dia 13/04/2022";
-	   cout<<"\n [6] Questão 06 - Será liberada no dia 13/04/2022";
-	   cout<<"\n [7] Questão 07 - Números Primos";
+	   cout<<"\n [1] QuestÃ£o 01 - SerÃ¡ liberada no dia 06/04/2022";
+	   cout<<"\n [2] QuestÃ£o 02 - SerÃ¡ liberada no dia 06/04/2022";
+	   cout<<"\n [3] QuestÃ£o 03 - SerÃ¡ liberada no dia 11/04/2022";
+	   cout<<"\n [4] QuestÃ£o 04 - SerÃ¡ liberada no dia 11/04/2022";
+	   cout<<"\n [5] QuestÃ£o 05 - SerÃ¡ liberada no dia 13/04/2022";
+	   cout<<"\n [6] QuestÃ£o 06 - SerÃ¡ liberada no dia 13/04/2022";
+	   cout<<"\n [7] QuestÃ£o 07 - NÃºmeros Primos";
 	   cout<<"\n [8] FIM.";
-	   cout<<"\n\n\n\n ESCOLHA UMA OPÇÃO --> ";
+	   cout<<"\n\n\n\n ESCOLHA UMA OPÃ‡ÃƒO --> ";
 	   cin>>op;
 	  switch (op)
 	  {/*
 	    case  1:{
 	         system ("color 2");
-	         cout<<"\n Opção 01";
+	         cout<<"\n OpÃ§Ã£o 01";
 	         cout<<" \n APERTE UMA TECLA";
 	         getch(); // espera digitar algo
 	         break;
 	           }
 	    case 2:{
              system ("color 3");
-             cout<<"\n Opção 02";
+             cout<<"\n OpÃ§Ã£o 02";
              cout<<" \n APERTE UMA TECLA";
              getch(); //ou system("pause");
         
@@ -69,56 +66,69 @@ cout << fixed << setprecision(2);
 	           }
 	    */
 	    case 7:{
-	    /* QUESTÃO 07:
-		Números Primos
-		Escolha uma opção:
-		[P] - Verifica se o informado é um Número Primo ou Não 
-		[L] - Mostra os números primos entre um intervalo de números fornecido pelo usuário. Peça o limite incial e o final.*/
+	    /* QUESTÃƒO 07:
+		NÃºmeros Primos
+		Escolha uma opÃ§Ã£o:
+		[P] - Verifica se o informado Ã© um NÃºmero Primo ou NÃ£o 
+		[L] - Mostra os nÃºmeros primos entre um intervalo de nÃºmeros fornecido pelo usuÃ¡rio. PeÃ§a o limite incial e o final.*/
 		
 	    	cout<<"\n =================================";
-			cout<<"\n == QUESTÃO 07 - NÚMEROS PRIMOS ==";
+			cout<<"\n == QUESTÃƒO 07 - NÃšMEROS PRIMOS ==";
 			cout<<"\n =================================";
-			cout<<"\n\n [P] - Verificar se é um Número Primo";
+			cout<<"\n\n [P] - Verificar se Ã© um NÃºmero Primo";
 			cout<<"\n [L] - Mostrar os Numeros Primos entre determinado intervalo";
-			cout<<"\n\n Selecione uma das opções acima >>>> \t";
+			cout<<"\n\n Selecione uma das opÃ§Ãµes acima >>>> \t";
 			cin>> escolha;
 			escolha=toupper(escolha);
-			
 			switch(escolha){
 				case 'P':{
-					cout<<"\n\n Insira um número para iniciarmos a verificação >>> \t";
+				    x=0;
+				    do{
+					cout<<"\n\n Insira um nÃºmero para iniciarmos a verificaÃ§Ã£o >>> \t";
 					cin>> numero;
-						for(int i=2;i=(numero/2);i++){
-							if(numero%i!=0){
-								cout<<"\n\n\t O número "<<numero<< " não é PRIMO!\n\n";	
-								break;
-							} else {
-								cout<<"\n\n\t O número "<<numero<< " está na casa dos PRIMOS!\n\n";
-								break;
+						for(int i=1;i<=numero;i++){
+							if(numero%i==0){
+								contador++;
 							}
-
 						}
+		        	if (contador==2){
+                       cout<<"\n\n\t" <<numero<< " Ã© um nÃºmero PRIMO. \t"; 
+                    } else {
+                       cout<<"\n\n\t" <<numero<< " nÃ£o Ã© um nÃºmero PRIMO. \t";
+                    }
+                    
+                    cout<<"\n\n >> Deseja verificar outro nÃºmero? [S]SIM  [N]NÃƒO \t";
+                    cin>> repetir;
+                    repetir=toupper(repetir);
+                        if (repetir=='S'){
+                            x=1;
+                        } else if (repetir=='N'){
+                            x=2;
+                        }
+				    } while ((x==0)||(x==1));
 					system("pause");
 					break;
 				}
+				/* - CONTINUAR A PARTIR DAQUI
 				case 'L':{
-					cout<<"\n\n Insira um número para iniciar a amostragem >>> \t";
+					cout<<"\n\n Insira um nÃºmero para iniciar a seleÃ§Ã£o >>> \t";
 					cin>> inicio;
-					cout<<"\n\n Agora defina um limite para sua seleção >>> \t";
+					cout<<"\n\n Agora defina um limite para sua seleÃ§Ã£o >>> \t";
 					cin>> fim;
-					
-						for(int i=inicio;i<fim;i++){
-							cout<<"\n\n Insira o "<<i<<"º número >>> \t";
-							cin>> numero;
-									do{
-										cout<< " " << numero << " ";
-										numero++;
-									} while ((numero%numero==0)&&(numero%1==0));
-								}	
+						for(int i=inicio;i<=fim;i++){
+						    
+						    do{ 
+						        cout<< " " << i << " ";
+						        i++;
+						    }while(fim);
 						}
-					system("pause");
-					break;
-				}
+						system("pause");
+					    break;	
+						}
+						
+				system("pause");
+				break;	
+				}*/
 			}
 			break;
 		
@@ -129,7 +139,7 @@ cout << fixed << setprecision(2);
              break;
 	            }
 	    default: {
-	         cout<<"\n Escolha Inválida! Tente novamente >>> ";
+	         cout<<"\n Escolha InvÃ¡lida! Tente novamente >>> ";
 	         cin>> op;
 	         getch();
 	         break;
@@ -138,4 +148,3 @@ cout << fixed << setprecision(2);
 	  
 	} while(op!=8);
 }
-
