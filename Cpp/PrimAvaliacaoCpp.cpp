93e68a0f2deac8996911dc7b95c9161c48329ffe
Kemily Teixeira <101020416+kmlyteixeira@users.noreply.github.com>
2022-04-09 17:29:20 -0300

    Update PrimAvaliacaoCpp.cpp

--- Cpp/PrimAvaliacaoCpp.cpp
+++ Cpp/PrimAvaliacaoCpp.cpp
@@ -8,13 +8,14 @@ Data: 04/04/2022
 */
 
 #include <iostream>
+#include <string>
 #include <stdlib.h>
 #include <iomanip>
 #include <conio.h> // Para usar o getch();
 
-int i, x = 0, op, numero = 0, inicio, fim, contador, aux, nprimo, positivo, negativo;
+int i, x = 0, y = 0, op, numero = 0, inicio, fim, contador, aux, nprimo, positivo=0; 
+int negativo=0, qntEleitor, voto, braCoo, henCav, ianSom, rodHil, chrPra, priSup,somVot, sindico;
 char escolha, repetir, um, dois, tres, quatro, cinco, seis, sete, oito, opcao;
-bool valid = false;
 
 using namespace std;
 main()
@@ -38,8 +39,8 @@ main()
     {
         system("cls");
         cout << "\n ====================================================================================";
-        cout << "\n [1] Questão 01 - Invetigação Criminal";
-        cout << "\n [2] Questão 02 - Será liberada no dia 06/04/2022";
+        cout << "\n [1] Questão 01 - Investigação Criminal";
+        cout << "\n [2] Questão 02 - Eleição de Sindico";
         cout << "\n [3] Questão 03 - Será liberada no dia 11/04/2022";
         cout << "\n [4] Questão 04 - Será liberada no dia 11/04/2022";
         cout << "\n [5] Questão 05 - Será liberada no dia 13/04/2022";
@@ -277,13 +278,12 @@ main()
                 } while (x == 0);
 
                 cout << "\n\n Com base em suas respostas, você foi classificado como...";
-                getch();
 
-                if (positivo == 4)
+                if (positivo<4)
                 {
-                    cout << "\n\n========================";
-                    cout << "\n======= SUSPEITO =======";
-                    cout << "\n========================\n\n";
+                    cout << "\n\n===============================================";
+                    cout << "\n== INOCENTE... Até que se prove ao contrário ==";
+                    cout << "\n===============================================\n\n";
                 }
                 else if ((positivo >= 5) && (positivo <= 7))
                 {
@@ -291,36 +291,18 @@ main()
                     cout << "\n== POSSÍVEL CRIMINOSO ==";
                     cout << "\n========================\n\n";
                 }
-                else if (positivo == 8)
+                else if ((positivo == 8)&&(negativo == 0))
                 {
                     cout << "\n\n===========================";
                     cout << "\n==========ASSASSINO========";
-                    cout << "\n===========================";
-                    cout << "\n\n      ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¦¦¦¯¯¯¦¦¦¦¦¦¦¯¯¯¦¦¦¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¯¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¯¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦\n";
-                    cout << "      ¦¦¦++¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦++¦¦¦\n";
-                    cout << "      ¦¦¦¦++¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦++¦¦¦¦\n";
-                    cout << "      ¦¦¦¦++_____¦¦¦¦¦_____++¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¦¦¦¦¦¯¯¦¦_¦¦¯¯¦¦¦¦¦¦¦¦¦\n";
-                    cout << "      ¦¦¯-+¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦+-¯¦¦\n";
-                    cout << "      ¦¦_¦¦¦___¦¦¦¯¦¯¦¦¦___¦¦¦_¦¦\n";
-                    cout << "      ¦¦¦¦_-+¦¦¦¦¦¦¦¦¦¦¦¦¦+-_¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¦¦¦¦¦---------¦¦¦¦¦¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¦¦¦¦¯-+++++++-¯¦¦¦¦¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¦_¦¦¦+-------+¦¦¦_¦¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¦¦¦_¦¦¦¦¦¦¦¦¦¦¦_¦¦¦¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¦¦¦¦¦¦_______¦¦¦¦¦¦¦¦¦¦\n";
-                    cout << "      ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦\n\n";
+                    cout << "\n===========================\n\n";
+
                 }
-                else
+                else if ((positivo == 4)&&(negativo == 4))
                 {
-                    cout << "\n\n===============================================";
-                    cout << "\n== INOCENTE... Até que se prove ao contrário ==";
-                    cout << "\n===============================================\n\n";
+                    cout << "\n\n========================";
+                    cout << "\n======= SUSPEITO =======";
+                    cout << "\n========================\n\n";
                 }
                 opcao = 'N';
             } while (opcao == 'S');
@@ -329,10 +311,100 @@ main()
         }
         case 2:
         {
-            system("color 3");
-            cout << "\n Opção 02";
-            cout << " \n APERTE UMA TECLA";
-            getch(); // ou system("pause");
+            cout << "\n =====================================";
+            cout << "\n == QUESTÃO 02 - ELEIÇÃO de SINDICO ==";
+            cout << "\n ======= Edificio Cordas Bambas ======";
+            cout << "\n\n Insira a quantidade de ELEITORES: ";
+            cin >> qntEleitor;
+
+            cout << "\n >>> Edificio Cordas Bambas apresenta seus Candidatos <<< ";
+            cout << "\n\t [1] - Henry Cavill";
+            cout << "\n\t [2] - Ian Somerhalder";
+            cout << "\n\t [3] - Rodrigo Hilbert";
+            cout << "\n\t [4] - Chris Pratt";
+            cout << "\n\t [5] - Bradley Cooper";
+
+                for(i=1;i<=qntEleitor;i++){
+                    do{
+                        cout << "\n\n Eleitor de número ["<<i<<"] >> Insira o número que corresponde ao seu candidato. VOTE CONSIENTE! >> \t";
+                        cin >> voto;
+                            if(voto == 1){
+                                y = 1;
+                                henCav++;
+                            } else if(voto == 2){
+                                y = 1;
+                                ianSom++;
+                            } else if(voto == 3){
+                                y = 1;
+                                rodHil++;
+                            } else if (voto == 4){
+                                y = 1;
+                                chrPra++;
+                            } else if (voto == 5){
+                                y = 1;
+                                braCoo++;
+                            }
+                            else if ((voto <1)||(voto>5)||(voto=0)){
+                                y = 0;
+                                cout << "\n\n VOTO INVÁLIDO! Não são permitidos votos NULOS ou EM BRANCO! Tente novamente...";
+                            }
+                    } while (y = 0);
+
+                    if (henCav>ianSom&&henCav>rodHil&&henCav>chrPra&&henCav>braCoo){
+                        sindico = 1;
+                    }
+                        else if (ianSom>henCav&&ianSom>rodHil&&ianSom>chrPra&&ianSom>braCoo){
+                            sindico = 2;
+                        }
+                            else if (rodHil>henCav&&rodHil>ianSom&&rodHil>chrPra&&rodHil>braCoo){
+                                sindico = 3;
+                            }
+                                else if (chrPra>henCav&&chrPra>ianSom&&chrPra>rodHil&&chrPra>braCoo){
+                                    sindico = 4;
+                                }
+                                    else if (braCoo>henCav&&braCoo>ianSom&&braCoo>rodHil&&braCoo>chrPra){
+                                        sindico = 5;
+                                    }         
+                }
+            switch (sindico)
+            {
+            case 1:
+                cout << "\n\n O NOVO SINDICO ELEITO É: Henry Cavill" ;
+                break;
+            case 2:
+                cout << "\n\n O NOVO SINDICO ELEITO É: Ian Somerhalder" ;
+                break;
+            case 3:
+                cout << "\n\n O NOVO SINDICO ELEITO É: Rodrigo Hilbert" ;
+                break;
+            case 4:
+                cout << "\n\n O NOVO SINDICO ELEITO É: Chris Pratt" ;
+                break;
+            case 5:
+                cout << "\n\n O NOVO SINDICO ELEITO É: Bradley Cooper" ;
+            default:
+                break;
+            }
+
+            somVot = henCav+ianSom+rodHil+chrPra+braCoo;
+
+            cout << "\n\n ==========================";
+            cout << "\n === INDICES DA ELEIÇÃO ===";
+            cout << "\n ==========================";
+            cout << "\n\n Total de VOTOS: " <<somVot;
+            cout << "\n Total de ELEITORES: " <<qntEleitor;
+                if(somVot == qntEleitor){
+                   cout << " >>> ELEIÇÃO VALIDADA! <<<";
+                } else {
+                   cout << " >>> ELEIÇÃO ANULADA por DESCONFORMIDADE! <<<"; 
+                }
+            cout << "\n\n Total de Votos por Candidato";
+            cout << "\n\t [1] - Henry Cavill: " << henCav << " VOTOS";
+            cout << "\n\t [2] - Ian Somerhalder: " << ianSom << " VOTOS";
+            cout << "\n\t [3] - Rodrigo Hilbert: " << rodHil << " VOTOS";
+            cout << "\n\t [4] - Chris Pratt: " << chrPra << " VOTOS";
+            cout << "\n\t [5] - Bradley Cooper: " << braCoo << " VOTOS \n\n";
+            system("pause");
             break;
         }
 
@@ -376,12 +448,15 @@ main()
                     repetir = toupper(repetir);
                     if (repetir == 'S')
                     {
+                    	contador = 0;
                         x = 1;
                     }
                     else if (repetir == 'N')
                     {
                         x = 2;
-                    }
+                    } else {
+                    	cout << "\n\n >> OPÇÃO INVÁLIDA! << Finalizando programa...";
+					}
                 } while ((x == 0) || (x == 1));
                 system("pause");
                 break;
@@ -410,17 +485,17 @@ main()
                         }
                         if (nprimo == 0)
                         {
-                            cout << " " << i;
+                            cout << " | " << i;
                         }
                     }
                     nprimo = 0;
                 }
-                system("pause");
                 break;
             }
-                system("pause");
-                break;
             }
+            cout << "\n\n";
+            system("pause");
+            break;
         }
         case 8:
         {
